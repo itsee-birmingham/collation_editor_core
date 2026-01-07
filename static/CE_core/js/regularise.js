649a4111fd3a625afe68e90965a68eab3160c0cc
Cat Smith <910384+catsmith@users.noreply.github.com>
2026-01-07 19:11:41 +0000

    put the blank lines back in

--- static/CE_core/js/regularise.js
+++ static/CE_core/js/regularise.js
@@ -869,9 +869,11 @@ var RG = (function () {
         }
       }
       options.configs.algorithm_settings = algorithmSettings;
+
       if (Object.prototype.hasOwnProperty.call(CL.services, 'collatexHost')) {
         options.configs.collatexHost = CL.services.collatexHost;
       }
+
       if (output === 'add_witnesses' || output === 'remove_overlap') {
         options.configs.split_single_reading_units = true;
         resultCallback = function (data) {
